8696c8a2d055f91333ecda773f33e60f63623e11
Priz Almarinez <priz.almarinez@gmail.com>
2021-03-30 22:25:34 +0800

    - Updated to small functions

--- index.js
+++ index.js
@@ -1,66 +1,56 @@
 const axios = require('axios')
+const url = 'https://bitbucket.org/!api/2.0/snippets/tawkto/aA8zqE/4f62624a75da6d1b8dd7f70e53af8d36a1603910/files/webstats.json'
 
-const getUniqWebsite = async (start, end) => {
-  // fetch json data
-  const response = await axios.get('https://bitbucket.org/!api/2.0/snippets/tawkto/aA8zqE/4f62624a75da6d1b8dd7f70e53af8d36a1603910/files/webstats.json')
-  const dataEntries = response.data
-  // filter unique website ids
-  const websiteIds = dataEntries.map(o => o.websiteId)
-  const uniqueWebsiteIds = [...new Set(websiteIds)]
-  // sum the number of chats and missedChats
+const compTotalWebStats = (webIds, data, startDate, endDate) => {
   let stats = []
-  // let filteredData = []
+  let newData = {}
 
-  try {
-    uniqueWebsiteIds.forEach((websiteId) => {
+  webIds.forEach((websiteId) => {
 
-      const newData = {
-        websiteId: websiteId,
-        chats: 0,
-        missedChats: 0
-      }
+    newData = {
+      websiteId: websiteId,
+      totalNumberOfChats: 0,
+      totalNumberOfMissedChats: 0
+    }
 
-      let filteredData = dataEntries.filter(data => data.websiteId === websiteId)
+    let filteredData = data.filter(data => data.websiteId === websiteId)
 
-      if (start && end) {
-        filteredData = filteredData.filter(item => { const date = new Date(item.date); return date >= start && date <= end; })
-      }
-      // reduce
-      // newData.chats = filteredData.reduce((currentTotal, item) => {
-      //   return item.chats + currentTotal
-      // }, 0)
+    if (startDate && endDate) {
+      filteredData = filteredData.filter(item => { const date = new Date(item.date); return date >= startDate && date <= endDate; })
+    }
 
-      // newData.missedChats = filteredData.reduce((currentTotal, item) => {
-      //   return item.missedChats + currentTotal
-      // }, 0)
-
-      // forEach
-      filteredData.forEach((item) => {
-        newData.chats += item.chats
-        newData.missedChats += item.missedChats
-      })
-
-      stats = [...stats, newData]
+    // forEach
+    filteredData.forEach((item) => {
+      newData.totalNumberOfChats += item.chats
+      newData.totalNumberOfMissedChats += item.missedChats
     })
 
-    return stats
+    stats.push(newData)
+  })
+
+  return stats
+}
+
+const websiteStats = async (startDate, endDate) => {
+  try {
+    // fetch json data
+    const response = await axios.get(url)
+    const dataEntries = response.data
+    // filter unique website ids
+    const websiteIds = dataEntries.map(o => o.websiteId)
+    const uniqueWebsiteIds = [...new Set(websiteIds)]
+    // compute the total sum of chats and missedChats
+    return compTotalWebStats(uniqueWebsiteIds, dataEntries, startDate, endDate)
   } catch (error) {
-    console.log(error)
     return error
   }
 }
 
 const main = async () => {
   console.log('STATS without date range filter: ')
-  console.log(await getUniqWebsite())
+  console.log(await websiteStats())
   console.log('STATS with date range filter: ')
-  console.log(await getUniqWebsite(new Date(2019, 2, 1), new Date(2019, 3, 8)))
+  console.log(await websiteStats(new Date(2019, 2, 1), new Date(2019, 3, 8)))
 }
 
 main()
-
-
-
-
-
-
